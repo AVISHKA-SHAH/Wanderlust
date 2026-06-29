018daeef066f48e84fbb911564b7151e21d4020b
Delta Student <avishkashah3@gmail.com>
2026-06-29 23:17:16 +0530

    fix: handle deleted listing requests with flash message

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,9 +20,7 @@ main().then(() =>{
     console.log(err);
 });
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
-}
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -28,14 +29,43 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.static("public"));
+// app.use(express.static("public"));
+
+
+const sessionOptions = {
+    secret: "mysupersecretstring", 
+    resave: false, 
+    saveUninitialized: true,
+    cookie:{
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true
+    }
+};
+
 
 app.get("/", (req, res) =>{
     res.send("Hi, I am root");
 });
 
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) =>{
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+
+    next();
+});
+
+
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
+
 
 
 app.use((req, res, next) => {
--- classroom/server.js
+++ classroom/server.js
@@ -2,34 +2,85 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
 
-app.use(cookieParser("secretcode"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/getsignedcookie", (req, res) =>{
-    res.cookie("made-in", "India", {signed: true});
-    res.send("Done! Signed cookie sent");
+const sessionOptions = {
+    secret: "mysupersecretstring", resave: false, saveUninitialized: true
+};
+
+ app.use(session(sessionOptions));
+ app.use(flash());
+
+app.use((req, res, next) =>{
+    res.locals.successMsg = req.flash("success");
+    res.locals.errorMSg = req.flash("error");
+    next();
 });
 
-app.get("/verify", (req, res) =>{
-    console.log(req.signedcookies0);
-    res.send("Verified")
-})
+ app.get("/register", (req, res)=>{
+    let {name="anonymous"} = req.query;
+    req.session.name = name;
+    if(name==="anonymous"){
+        req.flash("error", "user not registered");
+    }else{
+    req.flash("success", "User registered successfully");
+    }
+    res.redirect("/hello");
+ });
 
-app.get("/getcookies", (req, res) =>{
-    res.cookie("greet", "hello");
-    res.cookie("madeIn", "India");
-    res.send("sent you some cookies");
-});
+ app.get("/hello", (req, res) =>{
+    res.render("page.ejs", {name: req.session.name});
+ }); 
 
 
-app.get("/", (req, res) =>{
-    let {name = "anonymous"} = req.cookies;
-    res.send(`Hi, ${name}`);
-});
+// app.get("/reqcount",(req, res)=>{
+//     if(req.session.count){
+//         req.session.count++;
+//     }else{
+//         req.session.count = 1;
+//     }
+    
+//     res.send(`You sent a request ${req.session.count} times`);
+// });
 
-app.use("/users", users);
-app.use("/posts", posts);
+
+// app.get("/test", (req, res) =>{
+//     res.send("test successful");
+// });
+
+// const cookieParser = require("cookie-parser");
+
+// app.use(cookieParser("secretcode"));
+
+// app.get("/getsignedcookie", (req, res) =>{
+//     res.cookie("made-in", "India", {signed: true});
+//     res.send("Done! Signed cookie sent");
+// });
+
+// app.get("/verify", (req, res) =>{
+//     console.log(req.signedcookies0);
+//     res.send("Verified")
+// })
+
+// app.get("/getcookies", (req, res) =>{
+//     res.cookie("greet", "hello");
+//     res.cookie("madeIn", "India");
+//     res.send("sent you some cookies");
+// });
+
+
+// app.get("/", (req, res) =>{
+//     let {name = "anonymous"} = req.cookies;
+//     res.send(`Hi, ${name}`);
+// });
+
+// app.use("/users", users);
+// app.use("/posts", posts);
 
 
 app.listen(3000, () =>{
